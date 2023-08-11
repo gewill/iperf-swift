3a36009f3be9f9e32006e52764b61b033ebd4c24
Ge Will <531sunlight@gmail.com>
2023-08-11 12:55:29 +0800

    Remove krzyzanowskim/OpenSSL

--- Package.swift
+++ Package.swift
@@ -12,14 +12,12 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/krzyzanowskim/OpenSSL", from: "1.0.0"),
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         .target(
             name: "IperfCLib",
-            dependencies: [
-                .product(name: "OpenSSL", package: "openssl")
-            ],
+            dependencies: [],
             path: "Sources/IperfCLib"
         ),
         .target(
